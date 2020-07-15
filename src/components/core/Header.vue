10d5e9f30c99196e2cf28c67e97383519fd40da0
Bernard Gresola <gresola_bernard@yahoo.com>
2020-07-15 13:33:55 +0800

    Fixed Back Buttons

--- src/components/core/Header.vue
+++ src/components/core/Header.vue
@@ -2,10 +2,17 @@
   <header>
     <div class="navbar navbar-dark bg-primary shadow-sm">
       <div class="container d-flex justify-content-between">
-        <a  href="#" v-on:click.prevent="changeRoute('')" class="btn btn-outline-light" v-if="route != baseUrl ">
-          <i class="fa fa-arrow-left"></i>
-        </a>
-        <a href="/admin" class="navbar-brand d-flex align-items-center">
+        <div v-if="route != '/etriage-dashboard/employees'">
+          <a href="#" v-on:click.prevent="changeRoute('employees')" class="btn btn-outline-light">
+            <i class="fa fa-arrow-left"></i>
+          </a>
+        </div>
+        <div v-if="route != baseUrl">
+          <a  href="#" v-on:click.prevent="changeRoute('')" class="btn btn-outline-light" v-if="route == '/etriage-dashboard/employees'">
+            <i class="fa fa-arrow-left"></i>
+          </a>
+        </div>
+        <a href="#" class="navbar-brand d-flex align-items-center">
           <strong>UERM ETriage Dashboard</strong>
         </a>
       </div>
